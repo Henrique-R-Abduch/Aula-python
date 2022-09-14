795d7f1c46cb91743b28d4f6ddf3a69fbe48a176
Henrique-R-Abduch <henriqueabduch@gmail.com>
2022-09-13 21:51:53 -0300

    Correção

--- Ex41.py
+++ Ex41.py
@@ -1,14 +1,20 @@
-# 40. Exibir os vinte primeiros valores da série de Bergamaschi. A série: 1, 1, 1, 3, 5, 9, 17, ...
-a = 1
-b = 1
-c = 1
-print(a)
-print(b)
-print(c)
-for i in range(3,20):
-    d = a + b + c
-    print(d)
-    a = b
-    b = c
-    c = d
- 
+#41. Calcular e exibir a soma dos “N” primeiros valores da seqüência abaixo. O valor “N” será digitado, deverá ser positivo, mas menor que cem. Caso o valor não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente. A seqüência: 2, 5, 10, 17, 26, ....
+
+
+n = int(input("Digite um valor para N: "))
+while(n <= 0 or n >= 100):
+    print("Valor inválido!")
+    n = int(input("Digite um valor para N: "))
+a = 2
+b = 5
+c = 10
+d = 17
+soma = 0
+for i in range(1, n + 1):
+    print(a, b, c, d)
+    soma = soma + a + b + c + d
+    a = a + 1
+    b = b + 2
+    c = c + 3
+    d = d + 4
+print("A soma dos N primeiros valores é: ", soma)
--- Ex41W.py
+++ Ex41W.py
@@ -1,3 +1,6 @@
+#41. Calcular e exibir a soma dos “N” primeiros valores da seqüência abaixo. O valor “N” será digitado, deverá ser positivo, mas menor que cem. Caso o valor não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente. A seqüência: 2, 5, 10, 17, 26, ....
+
+
 n = int (input('Digite um número inteiro menor que 100: '))
 
 while (n > 99 or n < 0): 
